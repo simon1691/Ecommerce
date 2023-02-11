62d28ed5af6b3d433c220a0f60af9c0dbfcfaa11
Simon Agudelo <simonagudelo93@gmail.com>
2023-02-11 15:14:01 -0500

    Item List component added

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Item from "./Item"
+const ItemList = () => {
+    return (
+        <span>
+            <Item></Item>
+        </span>
+    )
+}
+
+export default ItemList
